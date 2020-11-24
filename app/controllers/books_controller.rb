430d280b324db32b99493aa4c278e2156faceeaf
Ubuntu <ubuntu@ip-172-31-25-50.ec2.internal>
2020-11-24 06:55:42 +0000

    Add image on book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,12 @@
 class BooksController < ApplicationController
   before_action :authenticate_user! 
+  before_action :authenticate_user! 
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+  def create
+    @book = Book.new(book_params)
+    @book.image.attach(params[:book][:image])
+
+    respond_to do |format|
+      if @book.save
  end
  
